0aaa83f0a3e02ccb186f76fe8b7dba5a94590a2a
Tanishq <gtanishq93@gmail.com>
2022-10-01 21:19:34 +0530

    Swagger dependency annotation and logback file added to the git and application properties edited as per requirement

--- src/main/java/com/rpfb/portal/Reform/Portal/ReformPortalApplication.java
+++ src/main/java/com/rpfb/portal/Reform/Portal/ReformPortalApplication.java
@@ -1,10 +1,17 @@
 package com.rpfb.portal.Reform.Portal;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class ReformPortalApplication {
+	
+	private static final Logger log = (Logger) LoggerFactory.getLogger(ReformPortalApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReformPortalApplication.class, args);
