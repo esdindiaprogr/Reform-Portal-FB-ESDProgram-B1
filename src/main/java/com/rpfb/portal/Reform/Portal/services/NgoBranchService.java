aa55b11a4ecdbdb019d669fea2751c7bc7d4d9ab
SAHIL ALI <84962462+SahilAli8808@users.noreply.github.com>
2022-10-07 21:55:40 +0530

    Update NgoBranchService.java
    NgoBranchService Update

--- src/main/java/com/rpfb/portal/Reform/Portal/services/NgoBranchService.java
+++ src/main/java/com/rpfb/portal/Reform/Portal/services/NgoBranchService.java
@@ -1,13 +1,16 @@
 package com.rpfb.portal.Reform.Portal.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rpfb.portal.Reform.Portal.model.NgoBranch;
 import com.rpfb.portal.Reform.Portal.repo.NgoBranchRepository;
 
-
+@Service
 public class NgoBranchService {
-
+	private static final Logger log = LoggerFactory.getLogger(NgoBranchService.class);
 	@Autowired
 		private NgoBranchRepository repository;
 		
