52586e9334b7a6cca6576ee86c52885e21832662
Tanishq <gtanishq93@gmail.com>
2022-10-08 15:57:48 +0530

    Interest Area Complete API added to the build

---
+++ src/main/java/com/rpfb/portal/Reform/Portal/repo/InterestAreaRepository.java
@@ -0,0 +1,13 @@
+package com.rpfb.portal.Reform.Portal.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rpfb.portal.Reform.Portal.model.InterestArea;
+
+public interface InterestAreaRepository extends CrudRepository<InterestArea, Long> {
+
+
+	
+    
+   
+}
